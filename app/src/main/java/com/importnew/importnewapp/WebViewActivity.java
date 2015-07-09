a00bf2894291b54716208c2e1b562aa0839e7865
liuzhijun <lzjun567@gmail.com>
2015-07-09 09:36:35 +0800

    添加aboutactivity背景色

--- app/src/main/java/com/importnew/importnewapp/WebViewActivity.java
+++ app/src/main/java/com/importnew/importnewapp/WebViewActivity.java
@@ -3,6 +3,7 @@ package com.importnew.importnewapp;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
